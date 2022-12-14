3914e86dbc1ff79e0a814e394df7c8fe2c6fafbd
clark <clark_mitchelljr@hotmail.com>
2022-12-14 10:19:44 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import SearchForm from "./components/SearchForm";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="container">
       <SearchForm />
+      <Nav />
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {NavLink, Link } from "react-router-dom";
 
 const Nav = () => {
   <nav className="main-nav">
     <ul>
-      <Link></Link>
+      <li>
+        <NavLink to="cat">Cats</NavLink>
+      </li>
+      <li>
+        <NavLink to="dogs">Dogs</NavLink>
+      </li>
+      <li>
+        <NavLink to="computers">Computers</NavLink>
+      </li>
     </ul>
   </nav>;
 };
