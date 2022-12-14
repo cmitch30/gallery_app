52c4b14d68ed1cc7d7057b56de0824b40be012ba
clark <clark_mitchelljr@hotmail.com>
2022-12-14 10:06:57 -0700

    update

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  <nav className="main-nav">
+    <ul>
+      <Link></Link>
+    </ul>
+  </nav>;
+};
+
+export default Nav;
